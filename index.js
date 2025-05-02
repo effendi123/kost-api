91e6772fc0bf4dc0ee4bd654e84296ba91cb9da8
effendi123 <achmatnoereffendi@gmail.com>
2025-05-02 21:26:47 +0700

    Use dynamic port for Render deployment

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-const express = require('express');
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server berjalan di http://localhost:${PORT}`);
+});const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
