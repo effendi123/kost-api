e45c246762beb8af4d645db677a0b9549e4ad01c
effendi123 <achmatnoereffendi@gmail.com>
2025-05-03 00:03:01 +0700

    Perbaiki index.js dan tambahkan route

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
+const express = require('express');
+const app = express();
+
 const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('API kost aktif');
+});
+
 app.listen(PORT, () => {
   console.log(`Server berjalan di port ${PORT}`);
 });
